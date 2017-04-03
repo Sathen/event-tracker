a1d2aa481c661b98047930ebc0bad056d0b2a0ec
Oleksii Shkot <oleksii_shkot@epam.com>
2017-04-03 21:01:14 +0300

    all done

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,6 +1,5 @@
-function Events($scope $http){
-        $http.get('http://localhost:8080/events').
-        success(function(data){
-            $scope.events = data;
-        });
+function Events($scope, $http) {
+    $http.get('http://localhost:8080/events').success(function (data) {
+        $scope.events = data;
+    });
 }
